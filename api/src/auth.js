70206a6dd3e0aba4a9e092eb4e0d40375088a3d5
Pierre <pierre.palmer@epitech.eu>
2023-09-27 15:22:37 +0200

    correction d'un bug lors du loggin : le nom de User était en toLowercase alors qu'il est sauvegarder avec une majuscule sur mongo

--- api/src/auth.js
+++ api/src/auth.js
@@ -26,12 +26,14 @@ class Auth {
 
   async signin(req, res) {
     let { password, username } = req.body;
-    username = (username || "").trim().toLowerCase();
+
+    username = (username || "").trim();
 
     if (!username || !password) return res.status(400).send({ ok: false, code: EMAIL_AND_PASSWORD_REQUIRED });
 
     try {
       const user = await this.model.findOne({ name: username });
+      
       if (!user) return res.status(401).send({ ok: false, code: USER_NOT_EXISTS });
 
       const match = await user.comparePassword(password);
@@ -62,7 +64,7 @@ class Auth {
       const { password, username, organisation } = req.body;
 
       if (password && !validatePassword(password)) return res.status(200).send({ ok: false, user: null, code: PASSWORD_NOT_VALIDATED });
-
+      //retourner une erreur pour l'utilisateur pour rendre l'app plus compréhensible
       const user = await this.model.create({ name: username, organisation, password });
       const token = jwt.sign({ _id: user._id }, config.secret, { expiresIn: JWT_MAX_AGE });
       const opts = { maxAge: COOKIE_MAX_AGE, secure: config.ENVIRONMENT === "development" ? false : true, httpOnly: false };
